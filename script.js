405cf8f559fa249329e58c017890322ca57f2a0d
saro-farzamnia <sarofarzamnia0@gmail.com>
2024-11-02 17:49:07 -0700

    Number ,...

---
+++ script.js
@@ -0,0 +1,13 @@
+// const name="saro";
+// const lastName="farzamnia";
+// const fullName= name + lastName;
+// console.log(fullName);
+
+
+let fullName="     sarofarzamnia     ";
+console.log(fullName.trim())
+
+let name="saro";
+let lastName="farzamnia";
+let age=15;
+console.log(name,lastName,age);
